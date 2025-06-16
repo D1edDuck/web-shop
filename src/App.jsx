055bb134b98e3e0b155c595fb70c3a61355c0f4e
Igor <mazurenkoi70@gmail.com>
2025-06-16 16:23:21 +0300

    Start Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,93 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Categories from "./pages/Categories";
+import ProductDetails from "./pages/ProductDetails";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <>
+//         <Header />
+//         <Home />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "about",
+//     element: (
+//       <>
+//         <Header />
+//         <About />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "cart",
+//     element: (
+//       <>
+//         <Header />
+//         <Cart />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "categories",
+//     element: (
+//       <>
+//         <Header />
+//         <Categories />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "*",
+//     element: (
+//       <>
+//         <Header />
+//         <NotFound />
+//         <Footer />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "product",
+//     element: (
+//       <>
+//         <Header />
+//         <ProductDetails />
+//         <Footer />
+//       </>
+//     ),
+//   },
+// ]);
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "cart", element: <Cart /> },
+      { path: "categories", element: <Categories /> },
+      { path: "product", element: <ProductDetails /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
-  return <div></div>;
+  return (
+    <>
+      <h1>Header</h1>
+      <NavLink to={"/about"}>About</NavLink>
+      <NavLink to={"/cart"}>Cart</NavLink>
+      <NavLink to={"/"}>Home</NavLink>
+      <hr />
+    </>
+  );
 }
 
 export default Header;
